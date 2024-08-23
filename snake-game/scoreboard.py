cda9b5fe2e1f69c512945801238c34878784f009
syedanazneen25 <syedanazneen2501@gmail.com>
2024-08-23 11:05:46 -0700

    new

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -6,7 +6,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
+        with open("C:/Users/DELL/Desktop/python-projects/snake-game/data.txt") as data:
             self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
@@ -21,7 +21,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("data.txt", mode="w") as data:
+            with open("C:/Users/DELL/Desktop/python-projects/snake-game/data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
